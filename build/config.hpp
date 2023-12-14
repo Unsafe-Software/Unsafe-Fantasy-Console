71f48ca56902528db0f5d09979490dd185e41801
LeviLovie <107021730+LeviLovie@users.noreply.github.com>
2023-12-14 18:18:04 +0700

    Delete build directory

--- build/config.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-namespace Config {
-    std::string License = "Unsafe-Fantasy-Console  Copyright (C) 2023 LeviLovie\nThis program comes with ABSOLUTELY NO WARRANTY;\nThis is free software, and you are welcome to redistribute it\nbut the above copyright notice and this permission notice\nshall be included in all copies or substantial portions of the Software.";
-    std::string Name = "\x1b[1mUnsafe-Fantasy-Console\x1b[0m";
-    std::string Version = "\x1b[1mv0.0.0\x1b[0m";
-}
